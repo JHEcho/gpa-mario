db614898a085c2efdd229e81b4e534ca8549b8c9
JHEcho <pyleso@natel.com>
2018-09-05 00:29:22 +0900

    master

---
+++ Assets/UI/Script/MainMenu/MainMenuUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour {
+
+    public void StartBegin()
+    {
+        SceneManager.LoadScene("StoryPage");
+    }
+    public void StartLoad()
+    {
+        SceneManager.LoadScene("Room");
+    }
+}
