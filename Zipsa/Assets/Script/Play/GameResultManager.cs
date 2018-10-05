e6f295840c63e3bbe2ed157cd874ed80a3fb89db
KimJangHwan0 <zzz6636@nate.com>
2018-10-05 17:22:28 +0900

    게임 일시정지, 게임 승리, 패배 구현

---
+++ Zipsa/Assets/Script/Play/GameResultManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameResultManager : MonoBehaviour {
+
+    public GameObject victoryPopup;
+    public GameObject defeatPopup;
+
+    private Timer timer;
+
+    // Use this for initialization
+    void Start () {
+        timer = GameObject.Find("GameTimerControl").GetComponent<Timer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Victory();
+        Defeat();
+	}
+
+    private void Victory()
+    {
+
+    }
+
+    private void Defeat()
+    {
+        if(timer.time < 0)
+        {
+            defeatPopup.SetActive(true);
+        }
+    }
+
+    public void ClosePopup(bool popup) //true = 승리, false = 패배.
+    {
+        if (popup)
+        {
+            victoryPopup.SetActive(false);
+            SceneManager.LoadScene("RoomScene");
+        }
+        else
+        {
+            defeatPopup.SetActive(false);
+            SceneManager.LoadScene("RoomScene");
+        }
+    }
+}
---
+++ Zipsa/Assets/Script/Play/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed;
+    public Joystick joystick;
+
+    /* Collider */
+    private BoxCollider2D boxCollider;
+    /* Check Collider */
+    public LayerMask layerMask;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveJoystick();
+    }
+
+    void MoveJoystick()
+    {
+        RaycastHit2D hit;
+
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical);
+
+        if (moveVector != Vector3.zero)
+        {
+            Vector2 currentPosition = transform.position;
+            //Vector2 resultPosition = currentPosition + new Vector2((moveVector.x * moveSpeed * (walkSize) / 1000), (moveVector.y * moveSpeed * (walkSize) / 1000));
+            Vector2 resultPosition = currentPosition + new Vector2((moveVector.x * moveSpeed * Time.deltaTime), (moveVector.y * (moveSpeed*2) * Time.deltaTime));
+
+            boxCollider.enabled = false;
+            hit = Physics2D.Linecast(currentPosition, resultPosition, layerMask);
+            boxCollider.enabled = true;
+
+            if(hit.transform == null)
+            //Rotation 정보
+            //transform.rotation = Quaternion.LookRotation(Vector3.forward, moveVector);
+            transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
